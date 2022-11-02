f1052b5c994c560f9cfe041d8b356568ba1ea64b
akhad <akhadyulchiev@gmail.com>
2022-11-02 11:24:16 +0500

    feat: import url updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://uj4lfgmija.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://2qd38yk130.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
